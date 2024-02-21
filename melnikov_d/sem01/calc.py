2f5a42131c8b809e98256447312f413484390434
DVMelnikov <mit23560@gmail.com>
2024-02-21 21:11:12 +0300

    sem1 init

---
+++ melnikov_d/sem01/calc.py
@@ -0,0 +1,11 @@
+def sum(a, b):
+    print(a + b)
+
+def diff(a, b):
+    print(a - b)
+
+def main():
+    print("Hi, iam calc, please use my methods in other packages")
+
+if __name__ == "__main__":
+    main()
