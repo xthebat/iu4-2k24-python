9bacbfaeef8807e37f3303423cb6535119674251
incano <incano@gmail.com>
2024-02-28 18:35:48 +0300

    added task01

---
+++ dychakovskaya_a/task01/main.py
@@ -0,0 +1,10 @@
+from function import function
+
+def main():
+    print (f"Введите аргумент функции:")
+    arg = input()
+    print (f"Вывод:")
+    function (arg)
+
+if __name__ == '__main__':
+    main()
