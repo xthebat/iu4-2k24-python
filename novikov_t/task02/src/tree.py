2085742d4725bcc19c4deb0a5ae2801266f2630d
Timofei <tictacnumt@gmail.com>
2024-03-02 20:09:40 +0300

    added pytest

--- novikov_t/task02/src/tree.py
+++ novikov_t/task02/src/tree.py
@@ -2,14 +2,14 @@ import os
 import argparse
 
 
-def generate_tree(directory, req_depth, current_depth=0, decor_prefix=""):
+def generate_tree(directory, req_depth, current_depth = 1, decor_prefix=""):
     if current_depth > req_depth:
         return
     # print directory name
     cur_dir = (decor_prefix + os.path.basename(directory))
     if cur_dir == "":           # user used a relative path
         print(os.path.basename(os.path.abspath(directory)))
-    elif current_depth == 0:    # user used an absolute path
+    else:                       # user used an absolute path
         print(cur_dir)
     # print the list of the items contained in directory
     list_of_items = os.listdir(directory)
@@ -32,12 +32,12 @@ def generate_tree(directory, req_depth, current_depth=0, decor_prefix=""):
             generate_tree(item_path, req_depth,
                           current_depth + 1, new_decor_prefix)
 
+if __name__ == "__main__" :
+    parser = argparse.ArgumentParser(
+        description="displays the directory structure as a tree with a specified depth")
+    parser.add_argument("-L", "--depth", type=int, default=1,
+                        help="Depth (nesting level) to display")
+    parser.add_argument("directory", type=str, help="Path to the directory")
 
-parser = argparse.ArgumentParser(
-    description="displays the directory structure as a tree with a specified depth")
-parser.add_argument("-L", "--depth", type=int, default=1,
-                    help="Depth (nesting level) to display")
-parser.add_argument("directory", type=str, help="Path to the directory")
-
-args = parser.parse_args()
-generate_tree(args.directory, args.depth)
+    args = parser.parse_args()
+    generate_tree(args.directory, args.depth)
