3bdcb56189f487d8efc8ef7663559dc64750a343
UniFav <robnur@mail.ru>
2024-03-03 20:16:58 +0300

    modified:   Nurumov_r/task02/src/task02/tree.py

--- Nurumov_r/task02/src/task02/tree.py
+++ Nurumov_r/task02/src/task02/tree.py
@@ -6,6 +6,11 @@ down = chr(9553)
 right = chr(9562)
 flat = chr(9552)
 
+# Colors for console output
+white = "\033[0m"
+blue = "\033[34m"
+green = "\033[32m"
+
 
 # Function to collect directories and files to a list
 def collect_tree(lvl: int, path: str) -> list:
@@ -17,10 +22,10 @@ def collect_tree(lvl: int, path: str) -> list:
             if lvl > 0:
                 elements.insert(0, collect_tree(
                     lvl, os.path.join(path, element)))
-            elements.insert(0, f"\033[34m{element}")
+            elements.insert(0, f"{blue}{element}")
 
         else:
-            elements.append(f"\033[32m{element}")
+            elements.append(f"{green}{element}")
 
     return elements
 
@@ -37,28 +42,28 @@ def tree(arrays: list, lvl: int) -> int:
 
         else:
             if lvl > 0:
-                print(f"\033[0m{down}", end='')
+                print(f"{white}{down}", end='')
                 for i in range(lvl):
                     print("   ", end='')
                     if i == lvl-1:
                         if element == arrays[-1]:
-                            print(f"\033[0m{right}", end='')
+                            print(f"{white}{right}", end='')
                         else:
-                            print(f"\033[0m{down_right}", end='')
+                            print(f"{white}{down_right}", end='')
                     else:
-                        print(f"\033[0m{down}", end='')
+                        print(f"{white}{down}", end='')
             else:
                 if element == arrays[-1]:
-                    print(f"\033[0m{right}", end='')
+                    print(f"{white}{right}", end='')
                 else:
-                    print(f"\033[0m{down_right}", end='')
+                    print(f"{white}{down_right}", end='')
 
-            if "\033[34m" in element:
+            if "{blue}" in element:
                 dirs += 1
             else:
                 files += 1
             for i in range(2):
-                print(f"\033[0m{flat}", end='')
+                print(f"{white}{flat}", end='')
             print(f" {element}")
 
     return dirs, files
@@ -67,11 +72,11 @@ def tree(arrays: list, lvl: int) -> int:
 def main(lvl: int, path: str):
     collection = collect_tree(lvl, path)
 
-    print(f"\033[34m{path}")
+    print(f"{blue}{path}")
     count = tree(collection, 0)
 
     count_str = f"\n{count[0]} directories, {count[1]} files"
-    print(f"\033[0m{count_str}")
+    print(f"{white}{count_str}")
 
 
 if __name__ == "__main__":
