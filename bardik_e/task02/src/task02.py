571f0115a4b4dd6129041da72fb6dad710015b49
Eduard Bardik <51379469+bkeduu@users.noreply.github.com>
2024-03-04 13:04:30 +0300

    review edits

--- bardik_e/task02/src/task02.py
+++ bardik_e/task02/src/task02.py
@@ -3,20 +3,15 @@ import os
 from task02 import tree
 
 if __name__ == "__main__":
-
-	# initializing argument parser object and adding needed params
 	parser = argparse.ArgumentParser(description="Tree - recursive ls.")
 	parser.add_argument("path", type=str, default=os.getcwd(),
 		nargs=1, help="Path to directory that will be recursively scanned.")
 	parser.add_argument("-d", "--depth", type=int, default=-1,
 		nargs=1, help="Maximum depth of tree.")
 	args = parser.parse_args()
+	path = args.path[0]
 
-	# if presented string is not a valid path, raise an error
-	if not os.path.isdir(args.path[0]):
+	if not os.path.isdir(path):
 		parser.error("provided string is not a valid path")
 
-	# if tree depth is not provided, args.depth will be int and == -1
-	# if tree depth is provided, args.depth will be list with 1 item
-	# call start function with this params
-	tree.run(args.path[0], int(args.depth[0] if isinstance(args.depth, list) else args.depth))
+	tree.run(path, int(args.depth[0] if isinstance(args.depth, list) else args.depth))
--- bardik_e/task02/src/task02/tree.py
+++ bardik_e/task02/src/task02/tree.py
@@ -38,43 +38,49 @@ def list_dir(path: str, current_depth: int, start_depth: int, prefix: str) -> No
 
 		except (PermissionError, FileNotFoundError):
 			# if it was unsuccessfully, printing notice with red font
-			print(f"{prefix}{ELBOW}{Fore.RED}PERMISSION DENIED{Style.RESET_ALL}")
+			print_entry("PERMISSION DENIED", "", prefix, True)
 
 		else:
 			# else, iterating over directory items
 			for item in dir_items:
-				print(prefix, end="")
 				full_path = os.path.join(path, item)
+				print_entry(item, path, prefix, item == dir_items[-1])
+				addition = PIPE if item != dir_items[-1] else BLANK
 
-				# checking if current item last or not last in entry list,
-				# printing appropriate prefix symbol
-				# and setting prefix addition symbol
-				if item != dir_items[-1]:
-					print(TEE, end="")
-					addition = PIPE
-				else:
-					print(ELBOW, end="")
-					addition = BLANK
-
-				# print special escape-chars for colorized output
-				if os.path.isdir(full_path):
-					print(Fore.BLUE, end="")
-				elif os.path.islink(full_path):
-					print(Fore.CYAN, end="")
-				else:
-					print(Fore.GREEN, end="")
-
-				# entry name
-				print(item, end="")
-
-				# if entry is a symlink, print link's source file path
-				if os.path.islink(full_path):
-					print(f" -> {os.path.realpath(full_path)}", end="")
-
-				# print special escape-char for colorized output end
-				print(Style.RESET_ALL)
-
-				# if entry is a directory, call this func recursively
-				# with prefix updated with addition
 				if os.path.isdir(full_path):
 					list_dir(full_path, current_depth + 1, start_depth, prefix + addition)
+
+
+def print_entry(name: str, path_prefix: str, prefix: str, is_last: bool) -> None:
+	"""
+
+	:param name: name of object in parent directory
+	:param path_prefix: full path to parent directory
+	:param prefix: print prefix
+	:param is_last: flag, that set to true when object is last in parent directory
+	"""
+	full_path = os.path.join(path_prefix, name)
+
+	print(f"{prefix}{ELBOW if is_last else TEE}", end="")
+	print(f"{get_output_color(full_path)}{name}", end="")
+
+	if os.path.islink(full_path):
+		print(f" -> {os.path.realpath(full_path)}", end="")
+
+	print(Style.RESET_ALL)
+
+
+def get_output_color(path: str) -> str:
+	"""
+
+	:param path: path to colorizing object
+	:return: ANSI-code for colorized output
+	"""
+	if os.path.isdir(path):
+		return Fore.BLUE
+	elif os.path.islink(path):
+		return Fore.CYAN
+	elif os.path.isfile(path):
+		return Fore.GREEN
+	else:
+		return Fore.RED
