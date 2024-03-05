230c2671a8694233c0aba2318e9b53ce22998d3e
sladost1 <petrichencko.daria@yandex.ru>
2024-03-05 21:43:27 +0300

    task01

---
+++ petrichenko_d/task01/main.py
@@ -0,0 +1,15 @@
+import z1
+
+z1
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    a = 10
+    b = 20
+    c = 30
+    print(f"c = a + b = {c}")
+    main()
---
+++ petrichenko_d/task01/z1.py
@@ -0,0 +1,3 @@
+if __name__ == "z1":
+    print(f"{__name__}")
+    print("da_petri")
